56a473acff4779b0d40bb5d089d5556b76b4dd66
Jean Lukas Wagner <jean-lukas.wagner@pulsvario.de>
2026-01-11 15:11:51 +0100

    Anpassung Impressum, try some fixes

--- assets/js/Bottom_Grid.js
+++ assets/js/Bottom_Grid.js
@@ -21,7 +21,7 @@ class Meinfooter extends HTMLElement {
             <a href="https://www.facebook.com/chor.x.wien" target="_blank" title="Facebook"><svg viewBox="0 0 24 24" width="28" height="28" fill="currentColor">
                 <path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.324v-21.35c0-.732-.593-1.325-1.325-1.325z"/>
             </svg></a>
-            <a href="mailto:chor.x@gmx.at" title="E-Mail"><svg viewBox="0 0 24 24" width="28" height="28" fill="currentColor">
+            <a href="mailto:wirsindchorx@gmail.com" title="E-Mail"><svg viewBox="0 0 24 24" width="28" height="28" fill="currentColor">
                 <path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l1.324 1.072c.338.274.738.411 1.138.411s.799-.137 1.138-.411l1.324-1.072 5.008 6.336h-14.94l5.008-6.336zm8.163-1.259l4.661-3.776v9.522l-4.661-5.746z"/>
             </svg></a>
         </div>
